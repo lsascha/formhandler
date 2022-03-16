1d29211d91eeecf19d2c4b643b2f1942cd2815c8
Gert Hammes <gert.hammes@brain-appeal.com>
2022-03-16 18:17:48 +0100

    [TASK] use Icons.php file

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'formhandler-foldericon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:formhandler/Resources/Public/Images/pagetreeicon.png',
+    ],
+];
